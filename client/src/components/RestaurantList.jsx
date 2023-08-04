45468269bdbb791f63f980edcbadd6a4cb0be3cb
Anjali Dubey <AKDdubeyanjali9819@gmail.com>
2023-08-04 23:26:37 +0530

    update

---
+++ client/src/components/RestaurantList.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const RestaurantList = () => {
+  return (
+    <div className="list-group">
+      <table className="table table-hover table-dark">
+        <thead>
+          <tr className="bg-primary">
+            <th scope="col">Restaurant</th>
+            <th scope="col">Location</th>
+            <th scope="col">Price Range</th>
+            <th scope="col">Ratings</th>
+            <th scope="col">Edit</th>
+            <th scope="col">Delete</th>
+          </tr>
+        </thead>
+      </table>
+    </div>
+  );
+};
+
+export default RestaurantList;
