a41a23daa7125071a49f6f4c0e68070cde0e152a
Anjali Dubey <AKDdubeyanjali9819@gmail.com>
2023-08-04 12:28:24 +0530

    update index.js

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const db = require("./db")
 //const morgan = require("morgan");
 const app = express();
 
@@ -8,24 +9,35 @@ const app = express();
 app.use(express.json());
 
 //Get all restaurants
-app.get("/api/v1/restaurants", (req, res) => {
+app.get("/api/v1/restaurants", async (req, res) => {
+  try {
+    const results = await db.query("select * from restaurants");
     res.status(200).json({
       status: "success",
+      results: results.rows.length,
       data: {
-        restaurant: ["wendys","mcdonalds"],
+        restaurants: results.rows,
       },
     });
+  } catch(err) {
+    console.log(err);
+  }
 });
 
 //Get a Restaurant
-app.get("/api/v1/restaurants/:id", (req,res) => {
-  console.log(req.params.id);
-  res.status(200).json({
-    status: "success",
-    data: {
-      restaurant:"wendys",
-    },
-  });
+app.get("/api/v1/restaurants/:id", async(req,res) => {
+  try {
+    const results = await db.query("select * from restaurants where id = $1", [req.params.id]);
+    //query will be - select * from restaurants where id = req.params.id
+    res.status(200).json({
+      status: "success",
+      data: {
+        restaurant: results.rows[0],
+      },
+    });
+  } catch(err) {
+    console.log(err);
+  }
 });
 
 //Create a restaurant
