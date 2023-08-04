03156097f934387f79fcc05761e545237427bda4
Anjali Dubey <AKDdubeyanjali9819@gmail.com>
2023-08-04 23:37:28 +0530

    update

--- client/src/routes/Home.jsx
+++ client/src/routes/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../components/Header';
 import AddRestaurant from '../components/AddRestaurant';
+import RestaurantList from '../components/RestaurantList';
 
 const Home = () => {
   return (
     <div>
       <Header/>
       <AddRestaurant/>
+      <RestaurantList/>
     </div>
   )
 }
