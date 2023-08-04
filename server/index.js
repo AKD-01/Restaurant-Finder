88087751f47d4fc96a3b155b665e54b1e5830558
Anjali Dubey <AKDdubeyanjali9819@gmail.com>
2023-08-04 13:12:03 +0530

    update index.js

--- server/index.js
+++ server/index.js
@@ -41,34 +41,45 @@ app.get("/api/v1/restaurants/:id", async(req,res) => {
 });
 
 //Create a restaurant
-app.post("/api/v1/restaurants", (req, res) => {
-  console.log(req.body);
-  res.status(201).json({
-    status: "success",
-    data: {
-      restaurant: "mcdonalds",
-    },
-  })
+app.post("/api/v1/restaurants", async(req, res) => {
+  try{
+    const results = await db.query("INSERT INTO restaurants (name, location, price_range) VALUES ($1, $2, $3) returning *", [req.body.name, req.body.location, req.body.price_range]);
+     res.status(201).json({
+       status: "success",
+       data: {
+         restaurant: results.rows[0],
+       },
+     });
+  } catch(err) {
+    console.log(err);
+  }
 });
 
 //Update a Restaurant
-app.put("/api/v1/restaurants/:id", (req, res) => {
-  console.log(req.params.id);
-  console.log(req.body);
-  res.status(200).json({
-    status: "success",
-    data: {
-      restaurant: "mcdonalds",
-    },
-  });
+app.put("/api/v1/restaurants/:id", async(req, res) => {
+  try {
+    const results = await db.query("UPDATE restaurants SET name=$1, location=$2, price_range=$3 WHERE id=$4 returning *",[req.body.name, req.body.location, req.body.price_range, req.params.id]);
+     res.status(200).json({
+       status: "success",
+       data: {
+         restaurant: results.rows[0],
+       },
+     });
+  } catch(err) {
+    console.log(err);
+  }
 });
 
 //Delete a Restaurant
-app.delete("/api/v1/restaurants/:id", (req,res) => {
-  console.log(req.params.id);
-  res.status(204).json({
-    status: "success",
-  });
+app.delete("/api/v1/restaurants/:id", async(req,res) => {
+  try {
+    const results = await db.query("DELETE FROM restaurants WHERE id=$1",[req.params.id]);
+    res.status(204).json({
+      status: "success",
+    });
+  } catch(err) {
+    console.log(err);
+  }
 });
 
 const port = process.env.PORT || 4000;
