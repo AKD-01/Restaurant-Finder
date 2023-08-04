61565a7337194462c53f7a0a49689c2647b4dd0d
Anjali Dubey <AKDdubeyanjali9819@gmail.com>
2023-08-04 14:38:00 +0530

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from "./routes/Home";
+import UpdatePage from "./routes/UpdatePage";
+import RestaurantDetailPage from "./routes/RestaurantDetailPage";
 
 const App = () => {
-    return (
-        <div>App</div>
-    );
-}
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/restaurants/:id" element={<RestaurantDetailPage />} />
+          <Route path="/restaurants/:id/update" element={<UpdatePage />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+};
 
 export default App;
