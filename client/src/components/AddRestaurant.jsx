d5cfd9082e938cafc07ef01eb7d69723846adb4c
Anjali Dubey <AKDdubeyanjali9819@gmail.com>
2023-08-04 23:18:31 +0530

    update

--- client/src/components/AddRestaurant.jsx
+++ client/src/components/AddRestaurant.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React from "react";
 
 const AddRestaurant = () => {
   return (
-    <div>
-      
+    <div className="mb-4">
+      <form action="">
+        <div className="form-row">
+          <div className="col">
+            <input type="text" className="form-control" placeholder="Name" />
+          </div>
+          <div className="col">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Location"
+            />
+          </div>
+          <div className="col">
+            <select className="custom-select my-1 mr-sm-2">
+              <option disabled>Price Range</option>
+              <option value="1">₹100-1000</option>
+              <option value="2">₹1000-2000</option>
+              <option value="3">₹2000-3000</option>
+              <option value="4">₹4000-5000</option>
+              <option value="5">₹6000-10000</option>
+            </select>
+          </div>
+          <button className="btn btn-primary">Add</button>
+        </div>
+      </form>
     </div>
-  )
-}
+  );
+};
 
 export default AddRestaurant;
