c5e70991f5fa656aa250b912b1070bb6d0eb6323
Anjali Dubey <AKDdubeyanjali9819@gmail.com>
2023-08-05 23:16:21 +0530

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,18 +3,21 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./routes/Home";
 import UpdatePage from "./routes/UpdatePage";
 import RestaurantDetailPage from "./routes/RestaurantDetailPage";
+import { RestaurantsContextProvider } from "./context/RestaurantsContext";
 
 const App = () => {
   return (
-    <div className="container">
-      <Router>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/restaurants/:id" element={<RestaurantDetailPage />} />
-          <Route path="/restaurants/:id/update" element={<UpdatePage />} />
-        </Routes>
-      </Router>
-    </div>
+    <RestaurantsContextProvider>
+      <div className="container">
+        <Router>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/restaurants/:id" element={<RestaurantDetailPage />} />
+            <Route path="/restaurants/:id/update" element={<UpdatePage />} />
+          </Routes>
+        </Router>
+      </div>
+    </RestaurantsContextProvider>
   );
 };
 
