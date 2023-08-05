251774bd216b71fbed4e2fce92b3f31334402c9c
Anjali Dubey <AKDdubeyanjali9819@gmail.com>
2023-08-06 00:08:49 +0530

    update

--- client/src/components/AddRestaurant.jsx
+++ client/src/components/AddRestaurant.jsx
@@ -1,22 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 const AddRestaurant = () => {
+  const [name, setName] = useState("");
+  const [location, setLocation] = useState("");
+  const [priceRange, setPriceRange] = useState("Price Range");
   return (
     <div className="mb-4">
       <form action="">
         <div className="form-row">
           <div className="col">
-            <input type="text" className="form-control" placeholder="Name" />
+            <input
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              type="text"
+              className="form-control"
+              placeholder="Name"
+            />
           </div>
           <div className="col">
             <input
+              value={location}
+              onChange={(e) => setLocation(e.target.value)}
               type="text"
               className="form-control"
               placeholder="Location"
             />
           </div>
           <div className="col">
-            <select className="custom-select my-1 mr-sm-2">
+            <select
+              value={priceRange}
+              onChange={(e) => setPriceRange(e.target.value)}
+              className="custom-select my-1 mr-sm-2"
+            >
               <option disabled>Price Range</option>
               <option value="1">₹100-1000</option>
               <option value="2">₹1001-2000</option>
@@ -25,7 +40,13 @@ const AddRestaurant = () => {
               <option value="5">₹5001-10000</option>
             </select>
           </div>
-          <button className="btn btn-primary">Add</button>
+          <button
+            onClick={handleSubmit}
+            type="submit"
+            className="btn btn-primary"
+          >
+            Add
+          </button>
         </div>
       </form>
     </div>
