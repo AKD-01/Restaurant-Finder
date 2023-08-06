b9e6c38782aa4a78353f9778ff5b17d29ecd0a26
Anjali Dubey <AKDdubeyanjali9819@gmail.com>
2023-08-06 19:33:05 +0530

    update

--- client/src/routes/RestaurantDetailPage.jsx
+++ client/src/routes/RestaurantDetailPage.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { useContext, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { RestaurantsContext } from "../context/RestaurantsContext";
+import RestaurantFinder from "../apis/RestaurantFinder";
 
 const RestaurantDetailPage = () => {
-  return (
-    <div>
-      Detail Page
-    </div>
-  )
-}
+  const { id } = useParams();
+  const {selectedRestaurant, setSelectedRestaurant} = useContext(RestaurantsContext);
+
+  useEffect(() => {
+    const fetchData = async() => {
+      const response = await RestaurantFinder.get(`/${id}`);
+      setSelectedRestaurant(response.data.data.restaurant);
+    }
+    fetchData();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  return <div>Detail Page</div>;
+};
 
 export default RestaurantDetailPage;
