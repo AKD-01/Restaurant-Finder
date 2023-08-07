f8a96ee231889dd2850d9c91132b3abc5e2bc4f7
Anjali Dubey <AKDdubeyanjali9819@gmail.com>
2023-08-07 09:27:59 +0530

    update

--- client/src/routes/RestaurantDetailPage.jsx
+++ client/src/routes/RestaurantDetailPage.jsx
@@ -19,6 +19,7 @@ const RestaurantDetailPage = () => {
     fetchData();
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
   return <div>{selectedRestaurant && selectedRestaurant.name}</div>;
 };
 
