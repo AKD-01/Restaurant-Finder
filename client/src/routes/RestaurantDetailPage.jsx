46fc3b05e77b6a725fcfd69eaee3a035c89b27dd
Anjali Dubey <AKDdubeyanjali9819@gmail.com>
2023-08-20 10:43:40 +0530

    update

--- client/src/routes/RestaurantDetailPage.jsx
+++ client/src/routes/RestaurantDetailPage.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import RestaurantFinder from "../apis/RestaurantFinder";
 import { RestaurantsContext } from "../context/RestaurantsContext";
+import StarRating from "../components/StarRating";
 
 const RestaurantDetailPage = () => {
   const { id } = useParams();
@@ -20,7 +21,7 @@ const RestaurantDetailPage = () => {
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  return <div>{selectedRestaurant && selectedRestaurant.name}</div>;
+  return <div>{selectedRestaurant && <StarRating rating={4} />}</div>;
 };
 
 export default RestaurantDetailPage;
