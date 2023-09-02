bc1c215a56f0a4a8f6c5fc7fb6c307a406e3618e
Anjali Dubey <AKDdubeyanjali9819@gmail.com>
2023-09-02 22:21:28 +0530

    update restaurantDetail.jsx

--- client/src/routes/RestaurantDetailPage.jsx
+++ client/src/routes/RestaurantDetailPage.jsx
@@ -2,26 +2,37 @@ import React, { useContext, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import RestaurantFinder from "../apis/RestaurantFinder";
 import { RestaurantsContext } from "../context/RestaurantsContext";
-import StarRating from "../components/StarRating";
+import Reviews from "../components/Reviews";
 
 const RestaurantDetailPage = () => {
   const { id } = useParams();
-  const {selectedRestaurant, setSelectedRestaurant} = useContext(RestaurantsContext);
+  const { selectedRestaurant, setSelectedRestaurant } =
+    useContext(RestaurantsContext);
 
   useEffect(() => {
-    const fetchData = async() => {
+    const fetchData = async () => {
       try {
         const response = await RestaurantFinder.get(`/${id}`);
         setSelectedRestaurant(response.data.data.restaurant);
-      } catch ( err) {
+      } catch (err) {
         console.log(err);
-      } 
-    }
+      }
+    };
     fetchData();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  return <div>{selectedRestaurant && <StarRating rating={2.3} />}</div>;
+  return (
+    <div>
+      {selectedRestaurant && (
+        <>
+          <div className="mt-3">
+            <Reviews />
+          </div>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default RestaurantDetailPage;
