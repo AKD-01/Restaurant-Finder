f23cac6c171bbd4c9a602b9efb023ee0c4dfa388
Anjali Dubey <AKDdubeyanjali9819@gmail.com>
2023-09-05 22:40:57 +0530

    update

--- client/src/components/Reviews.jsx
+++ client/src/components/Reviews.jsx
@@ -1,65 +1,27 @@
 import React from "react";
 import StarRating from "./StarRating";
 
-const Reviews = () => {
+const Reviews = ({ reviews }) => {
   return (
     <div className="row row-cols-3 mb-2">
-      <div
-        className="card text-white bg-primary mb-3 mr-4"
-        style={{ maxWidth: "30%" }}
-      >
-        <div className="card-header d-flex justify-content-between">
-          <span>Anjali</span>
-          <span>
-            <StarRating rating={3} />
-          </span>
-        </div>
-        <div className="card-body">
-          <p className="card-text">This restaurant was awesome</p>
-        </div>
-      </div>
-      <div
-        className="card text-white bg-primary mb-3 mr-4"
-        style={{ maxWidth: "30%" }}
-      >
-        <div className="card-header d-flex justify-content-between">
-          <span>Anjali</span>
-          <span>
-            <StarRating rating={3} />
-          </span>
-        </div>
-        <div className="card-body">
-          <p className="card-text">This restaurant was awesome</p>
-        </div>
-      </div>
-      <div
-        className="card text-white bg-primary mb-3 mr-4"
-        style={{ maxWidth: "30%" }}
-      >
-        <div className="card-header d-flex justify-content-between">
-          <span>Anjali</span>
-          <span>
-            <StarRating rating={3} />
-          </span>
-        </div>
-        <div className="card-body">
-          <p className="card-text">This restaurant was awesome</p>
-        </div>
-      </div>
-      <div
-        className="card text-white bg-primary mb-3 mr-4"
-        style={{ maxWidth: "30%" }}
-      >
-        <div className="card-header d-flex justify-content-between">
-          <span>Anjali</span>
-          <span>
-            <StarRating rating={3} />
-          </span>
-        </div>
-        <div className="card-body">
-          <p className="card-text">This restaurant was awesome</p>
-        </div>
-      </div>
+      {reviews.map((review) => {
+        return (
+          <div key={review.id}
+            className="card text-white bg-primary mb-3 mr-4"
+            style={{ maxWidth: "30%" }}
+          >
+            <div className="card-header d-flex justify-content-between">
+              <span>{review.name}</span>
+              <span>
+                <StarRating rating={review.rating} />
+              </span>
+            </div>
+            <div className="card-body">
+              <p className="card-text">{review.review}</p>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
