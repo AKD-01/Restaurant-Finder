032f0bc6321e573ae6ee827cb7f892f1af6bf547
Anjali Dubey <AKDdubeyanjali9819@gmail.com>
2023-09-05 22:41:06 +0530

    update

--- client/src/components/StarRating.jsx
+++ client/src/components/StarRating.jsx
@@ -4,12 +4,12 @@ const StarRating = ({ rating }) => {
   const stars = [];
   for (let i = 1; i <= 5; i++) {
     if (i <= rating) {
-      stars.push(<i class="fas fa-star text-warning"></i>);
+      stars.push(<i key={i} className="fas fa-star text-warning"></i>);
     } else if (i===Math.ceil(rating) && !Number.isInteger(rating)) {
-        stars.push(<i class="fas fa-star-half-alt text-warning"></i>)
+        stars.push(<i key={i} className="fas fa-star-half-alt text-warning"></i>)
     }
     else {
-      stars.push(<i class="far fa-star text-warning"></i>);
+      stars.push(<i key={i} className="far fa-star text-warning"></i>);
     }
   }
   return <>{stars}</>;
