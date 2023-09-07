f98ed1161f0366911c2d54bda55ef01344cebfef
Anjali Dubey <AKDdubeyanjali9819@gmail.com>
2023-09-07 13:43:14 +0530

    update reviews

--- client/src/routes/RestaurantDetailPage.jsx
+++ client/src/routes/RestaurantDetailPage.jsx
@@ -4,6 +4,7 @@ import RestaurantFinder from "../apis/RestaurantFinder";
 import { RestaurantsContext } from "../context/RestaurantsContext";
 import Reviews from "../components/Reviews";
 import AddReview from "../components/AddReview";
+import StarRating from "../components/StarRating";
 
 const RestaurantDetailPage = () => {
   const { id } = useParams();
@@ -27,7 +28,17 @@ const RestaurantDetailPage = () => {
     <div>
       {selectedRestaurant && (
         <>
-        <h1 className="text-center display-1">{selectedRestaurant.restaurant.name}</h1>
+          <h1 className="text-center display-1">
+            {selectedRestaurant.restaurant.name}
+          </h1>
+          <div className="text-center">
+            <StarRating rating={selectedRestaurant.restaurant.average_rating} />
+            <span className="text-warning ml-1">
+              {selectedRestaurant.restaurant.count
+                ? `(${selectedRestaurant.restaurant.count})`
+                : "(0)"}
+            </span>
+          </div>
           <div className="mt-3">
             <Reviews reviews={selectedRestaurant.reviews} />
           </div>
