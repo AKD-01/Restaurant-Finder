0ed09cfba8346e4ede6e45cda734b6b9aec31736
Anjali Dubey <AKDdubeyanjali9819@gmail.com>
2023-09-07 12:34:34 +0530

    update

--- client/src/components/AddReview.jsx
+++ client/src/components/AddReview.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useLocation, useNavigate, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import RestaurantFinder from "../apis/RestaurantFinder";
 
 const AddReview = () => {
